18d85649db93f56564b1bbbf1ef25d97a161f094
erosman <erosman@users.noreply.github.com>
2019-07-08 20:47:16 +0430

    Update pattern-tester.js

--- src/scripts/pattern-tester.js
+++ src/scripts/pattern-tester.js
@@ -1,58 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
-  $(document).on("click", "#test", testPattern);
-  $(document).on("click", "#help", () => browser.tabs.create({url: "/pattern-help.html", active: true}));
+'use strict';
 
-  let patternObj = Utils.urlParamsToJsonMap().patternObj;
+document.querySelector('#test').addEventListener('click', testPattern);
+document.querySelector('#help').addEventListener('click', () => browser.tabs.create({url: '/pattern-help.html'}));
 
-  if (patternObj) {
-    $("#pattern").val(patternObj.pattern);
-    $("#protocols").val(patternObj.protocols);
-    $("#type").val(patternObj.type);
-    $("#url").val("");
-  }
-});
+const patternObj = Utils.urlParamsToJsonMap().patternObj;
+if (patternObj) {
+  document.querySelector('#pattern').value = patternObj.pattern;
+  document.querySelector('#protocols').value = patternObj.protocols;
+  document.querySelector('#type.valu')e = patternObj.type;
+  document.querySelector('#url').value = '';
+}
+
 
 function testPattern() {
-  let urlInput = $("#url"), url = urlInput.val(), parsedURL;
-  urlInput.removeClass("is-invalid-input");
 
-  try {
-    parsedURL = new URL(url);
-  }
+  const urlInput = document.querySelector('#url');
+  urlInput.classList.remove('is-invalid-input');  // reset
+  let parsedURL;
+
+  try { parsedURL = new URL(urlInput.value); }
   catch (e) {
     console.error(e);
-    urlInput.addClass("is-invalid-input");
+    urlInput.classList.add('is-invalid-input');
     return false;
   }
-  if (!validateInput()) return;
-  let pattern = $("#pattern").val(), type = parseInt($("#type").val()), protocols = parseInt($("#protocols").val()),
-    regExp, schemeNum;
+
+  if (!validateInput()) { return; }
+
+  const pattern = document.querySelector('#pattern').value;
+  const type = parseInt(document.querySelector('#type').value);
+  const protocols = parseInt(document.querySelector('#protocols').value);
+  let schemeNum;
 
   // Check protocol first
-  if (parsedURL.protocol == "https:") schemeNum = PROTOCOL_HTTPS;
-  else if (parsedURL.protocol == "http:") schemeNum = PROTOCOL_HTTP;
+  if (parsedURL.protocol === 'https:') { schemeNum = PROTOCOL_HTTPS; }
+  else if (parsedURL.protocol === 'http:') { schemeNum = PROTOCOL_HTTP; }
 
-  if (protocols != PROTOCOL_ALL && protocols != schemeNum) {
-    console.log("no protocol match: " + schemeNum);
-    $("#match,#noMatch").hide();
-    $("#noProtocolMatch").show();
+  if (protocols !== PROTOCOL_ALL && protocols !== schemeNum) {
+    console.log('no protocol match: ' + schemeNum);
+    
+    // #match,#noMatch, #noProtocolMatch are already hidden via hide-unimportant
+    document.querySelector('#noProtocolMatch').classList.remove('hide-unimportant');
     return;
   }
 
-  if (type == PATTERN_TYPE_WILDCARD)
-    regExp = Utils.safeRegExp(Utils.wildcardStringToRegExpString(pattern));
-  else
-     regExp = Utils.safeRegExp(pattern); // TODO: need to notify user and not match this to zilch.
+  const regExp = type === PATTERN_TYPE_WILDCARD ? Utils.safeRegExp(Utils.wildcardStringToRegExpString(pattern)) :
+                            Utils.safeRegExp(pattern); // TODO: need to notify user and not match this to zilch.
 
-  if (regExp.test(parsedURL.host)) {
-    console.log("match");
-    $("#match").show();
-    $("#noMatch,#noProtocolMatch").hide();
-  }
-  else {
-    console.log("no match");
-    $("#match,#noProtocolMatch").hide();
-    $("#noMatch").show();
-  }
+  document.querySelector(regExp.test(parsedURL.host) ? '#match' : '#noMatch').classList.remove('hide-unimportant');
 }
-
