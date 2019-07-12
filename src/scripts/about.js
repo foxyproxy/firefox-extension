881e7d27857d0d1ba8ff97a8c4868f48b29fe88e
Eric Jung <eric.jung@getfoxyproxy.org>
2019-07-12 18:53:16 +0100

    fix colon syntax error. remove un-needed css and js

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,9 +1,5 @@
-// browser.management is only used here, 
-// you can also use browser.runtime.getManifest()
-// no JQuery used
-
 const manifest = browser.runtime.getManifest();
 
 document.querySelector('#version').textContent = manifest.version;
 document.querySelector('#edition').textContent = FOXYPROXY_BASIC ? 'FoxyProxy Basic' : 'FoxyProxy Standard';
-document.querySelector('#okBtn').addEventListener('click', () => location.href = '/proxies.html'):
+document.querySelector('#okBtn').addEventListener('click', () => location.href = '/proxies.html');
