1781b4c77d8e41ef9a7022261d2d68b5d0709367
Eric Jung <eric.jung@getfoxyproxy.org>
2019-07-12 20:43:41 +0100

    fix syntax error and minor spacing

--- src/scripts/validate-pattern.js
+++ src/scripts/validate-pattern.js
@@ -5,23 +5,21 @@ function validateInput() {
   return markInputErrors();
 }
 
-// Return false if any item in the selector is empty or doesn't have only nums when
-// |numbersOnly| is true
 function markInputErrors() {
-  
+
   const patInput = document.querySelector('#pattern');
   patInput.classList.remove('is-invalid-input'); // reset
   let pat = patInput.value;
-  
+
   if (!pat) {
     patInput.classList.add('is-invalid-input');
     return false;
   }
-  
+
   const type = parseInt(document.querySelector('#type').value);
   switch (true) {
 
-    case type === PATTERN_TYPE_WILDCARD && pat.indexOf('/') >== 0:
+    case type === PATTERN_TYPE_WILDCARD && pat.indexOf('/') >= 0:
       alert("No slash in wildcard patterns. You cannot match URL paths because of Firefox restrictions.");
       patInput.classList.add('is-invalid-input');
       return false;
