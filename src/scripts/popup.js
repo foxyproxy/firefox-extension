ecb63bd5198e00caea5d45ee882d9f311d2787ae
erosman <erosman@users.noreply.github.com>
2019-07-13 23:05:03 +0430

    Update popup.js
    - use strict
    - removed JQuery
    - commented out unused function, delete if not needed
    - using innerHTML & DOMPurify for now, to fix later

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,6 +1,9 @@
-getAllSettings().then(popupSuccess, popupError).catch((e) => console.log("exception: " + e));
+'use strict';
+
+getAllSettings().then(popupSuccess, popupError).catch((e) => console.log('exception: ' + e));
 
 function popupSuccess(settings) {
+  
   /*if (!proxySettings.length) {
     // Display defaults
     console.log("No proxies found in storage.");
@@ -14,68 +17,87 @@ function popupSuccess(settings) {
 }
 
 function popupError(error) {
+  
   console.log(`popupError(): ${error}`);
-  $("#spinnerRow").hide();
-  $("#errorRow").show();
+  // using hide-unimportant CSS to show/hide
+  document.querySelector('#spinnerRow').classList.add('hide-unimportant');
+  document.querySelector('#errorRow').classList.remove('hide-unimportant');
 }
 
 function renderOptions(settings) {
-  console.log("renderOptions() and settings is " + JSON.stringify(settings));
-  let rows = [], ids = [];
+  
+  console.log('renderOptions() and settings is ' + JSON.stringify(settings));
+  const rows = [], ids = [];
   settings.proxySettings.forEach((proxySetting) => {
-    let option = Utils.getOption(proxySetting);
+    const option = Utils.getOption(proxySetting);
     if (option) {
       rows.push(option);
       ids.push(proxySetting.id);
     }
   });
 
-  $("#menuInsertPoint").html(DOMPurify.sanitize(rows.join(""), {SAFE_FOR_JQUERY: true}));
-  let mode = settings.mode ? settings.mode : PATTERNS;
-  if (mode == PATTERNS) {
-    $("#patternsSelected").replaceWith('<i class="fa fa-check" style="color:green"></i>&nbsp;');
-  }
-  else if (mode == DISABLED) {
-    $("#disabledSelected").replaceWith('<i class="fa fa-check" style="color:green"></i>&nbsp;');
-  }
-  else {
-    $("#" + mode + "Selected").replaceWith('<i class="fa fa-check" style="color:green"></i>&nbsp;');
+  // using innerHTML & DOMPurify for now, to fix later
+  document.querySelector('#menuInsertPoint').innerHTML = DOMPurify.sanitize(rows.join(""), {SAFE_FOR_JQUERY: true});
+  
+
+
+  const mode = settings.mode ? settings.mode : PATTERNS;
+  // <span id="patternsSelected"></span>
+  // no need to replace node, using CSS on the same node
+  let target;
+  switch (mode) {
+
+    case 'PATTERNS': target = '#patternsSelected'; break;
+    case 'DISABLED': target = '#disabledSelected'; break;
+    default: target = '#' + mode + 'Selected';
   }
 
-  if (FOXYPROXY_BASIC) $("#patternsListItem").hide();
+  const node = document.querySelector(target);
+  node.className = 'fa fa-check';
+  node.setAttribute('style', 'color: green; font-style: italic; margin-right: 0.5em;');
+
+  FOXYPROXY_BASIC && document.querySelector('#patternsListItem').classList.add('hide-unimportant');
   installListeners(ids);
 
-  $("#spinnerRow").hide();
-  $("#optionsRow").show();
+  // using hide-unimportant CSS to show/hide
+  document.querySelector('#spinnerRow').classList.add('hide-unimportant');
+  document.querySelector('#optionsRow').classList.remove('hide-unimportant');
 }
 
 function installListeners(ids) {
-  $(document).on("click", "#options", function(evt) {
-    Utils.showInternalPage("proxies").then(() => window.close());
+  
+  document.querySelector('#options').addEventListener('click', (e) => {
+    Utils.showInternalPage('proxies').then(window.close);
+  });
+  
+  // no need for active: true, both FF & CH default to true
+  document.querySelector('#where').addEventListener('click', (e) => {
+    browser.tabs.create({url: 'https://getfoxyproxy.org/geoip/'}).then(window.close);
+  });  
+
+  document.querySelector('#log').addEventListener('click', (e) => {
+    Utils.showInternalPage('log').then(window.close);
   });
 
-  $(document).on("click", "#where", function(evt) {
-    browser.tabs.create({url: "https://getfoxyproxy.org/geoip/", active: true}).then(() => window.close());
+  document.querySelectorAll('#patterns, #disabled, #' + ids.join(', #')).forEach(item => {
+    item.addEventListener('click', function(e) { setMode(this.id).then(window.close); });
   });
-
-  $(document).on("click", "#log", function(evt) {
-    Utils.showInternalPage("log").then(() => window.close());
-  });
-
-
-  $("#patterns,#disabled,#" + ids.join(",#")).on("click",function(e) {
-    setMode($(this).attr("id")).then(() => { window.close() });
-  });
-
-  function enableOrDisable(active, evt) {
-    evt.preventDefault();
-    $("#optionsRow").hide();
-    $("#spinnerRow").show();
-    enableDisableAllProxySettings(active).then((proxySettings) => {
-      let windows = browser.extension.getViews({type: "tab"});
-      for (let w of windows)
-        w.location.reload();
-      popupSuccess(proxySettings);
-    }, popupError);
-  }
 }
+
+
+/*
+// This function was nested in above
+// It doesn't seem to be used anywhere
+
+function enableOrDisable(active, evt) {
+  evt.preventDefault();
+  $("#optionsRow").hide();
+  $("#spinnerRow").show();
+  enableDisableAllProxySettings(active).then((proxySettings) => {
+    let windows = browser.extension.getViews({type: "tab"});
+    for (let w of windows)
+      w.location.reload();
+    popupSuccess(proxySettings);
+  }, popupError);
+}
+*/
