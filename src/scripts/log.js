e3e25d36d8b47faf5207cdf929c8a89eca9660ad
erosman <erosman@users.noreply.github.com>
2019-07-14 14:18:38 +0430

    Update log.js
    - use strict
    - Remove JQuery
    - New localising Date & time
    - Code update
    - Note the comments made (to the dev) and edit where needed

--- src/scripts/log.js
+++ src/scripts/log.js
@@ -1,4 +1,136 @@
+'use strict';
+
 let logg;
+const onOff = document.querySelector('#onOff'); // cache for later
+
+browser.runtime.getBackgroundPage().then((page) => {
+  logg = page.getLogg();
+  //console.log("logg active is " + logg.active);
+  if (logg.active) {
+    onOff.checked = true;
+    renderLog();
+  }
+  else {
+    onOff.checked = false;
+    // using hide-unimportant class app.css#4575 to show/hide
+    document.querySelector('#spinnerRow').classList.add('hide-unimportant');
+    document.querySelector('#logRow').classList.remove('hide-unimportant');
+  }
+});
+
+onOff.addEventListener('click', (e) => {
+  const isON = onOff.checked;
+  //console.log("user changed logging to " + isON);
+  browser.runtime.getBackgroundPage().then((page) => {
+    page.ignoreNextWrite(); // Don't propagate changes the PAC script
+    setLogging(500, isON).then(() => {
+      if (!isON) {
+        logg.active = false;
+        logg.clear(); // maybe it is better not clearing the log, user might temporary want to diable, use clear to manually clear
+        renderLog();
+      }
+      else { logg.active = true; }
+      getAllSettings().then((s) => console.log(s));
+    });
+  });
+});
+
+
+document.querySelectorAll('input[type="button"]').forEach(item => item.addEventListener('click', process));
+
+function process () {
+
+  switch (this.id) {
+
+    case 'okBtn1':
+    case 'okBtn2':
+      location.href = '/proxies.html'
+      break;
+
+    case 'clearBtn1':
+    case 'clearBtn2':
+      logg.clear();
+      renderLog();
+      break;
+
+    case 'refreshBtn1':
+    case 'refreshBtn2':
+      renderLog();
+      break;
+
+  }
+}
+
+function renderLog() {
+
+  // ----- templates & containers
+  const docfrag = document.createDocumentFragment();
+  const tr = document.createElement('tr');
+  const td = document.createElement('td');
+  const a = document.createElement('a');
+  a.setAttribute('target', '_blank');
+  // create 6 td inside tr
+  for (let i = 1; i <= 6; i++) { tr.appendChild(td.cloneNode()); }
+
+
+  for (let i = 0, len = logg.length; i < len; i++) {
+
+    let item = logg.item(i), pattern;
+    if (item.matchedPattern) {
+      pattern = item.matchedPattern === USE_PROXY_FOR_ALL_URLS ? 'Use proxy for all URLs' :
+        item.matchedPattern.pattern;
+    }
+    else { pattern = 'No matches'; }
+
+    // Build a row for this log entry by cloning the tr containing 6 td
+    const row = tr.cloneNode(true);
+    row.className = item.matchedPattern ? 'success' : 'secondary';
+
+    const cell1 = row.children[0];
+    const a1 = a.cloneNode();
+    a1.href = item.url;
+    a1.textContent = item.url;
+    cell1.appendChild(a1);
+
+    const cell2 = row.children[1];
+    cell2.textContent = item.proxySetting ? item.proxySetting.title : 'No matches';
+
+    const cell3 = row.children[2];
+    cell3.className = 'fp-color-blob-log'; // this style is blank app.css#4675
+    cell3.style.backgroundColor = item.proxySetting ? item.proxySetting.color : 'blue';
+    cell3.style.border = '2px solid #fff'; // looks better separated, but not important
+
+    const cell4 = row.children[3];
+    cell4.textContent = item.proxySetting ? item.proxySetting.address : 'No matches';
+
+    const cell5 = row.children[4];
+    cell5.textContent = pattern
+
+    const cell6 = row.children[5];
+    cell6.textContent = formatInt(item.timestamp);
+
+    docfrag.appendChild(row);
+  }
+
+  const parent = document.querySelector('#logTable tbody'); // reduce the number of needed ids e.g. #rows
+  parent.textContent = ''; // clearing the content
+  parent.appendChild(docfrag);
+
+  // using hide-unimportant class app.css#4575 to show/hide
+  document.querySelector('#spinnerRow').classList.add('hide-unimportant');
+  document.querySelector('#logRow').classList.remove('hide-unimportant');
+}
+
+function formatInt(d) {
+  // International format based on user locale
+  // you can delete the other function if you like this
+  // you can adjust the content via the object properties
+  return new Intl.DateTimeFormat(navigator.language,
+                  {weekday: 'short', year: 'numeric', month: 'short', day: 'numeric',
+                    hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false}).format(new Date(d));
+}
+
+
 
 /*
 months.long.1=January
@@ -24,106 +156,15 @@ timeformat=HH:nn:ss:zzz mmm dd, yyyy
 */
 
 // TODO: i18n
-let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-  days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-document.addEventListener("DOMContentLoaded", function() {
-  browser.runtime.getBackgroundPage().then((page) => {
-    logg = page.getLogg();
-    console.log("logg active is " + logg.active);
-    if (logg.active) {
-      $("#onOff").prop("checked", true);
-      renderLog();
-    }
-    else {
-      $("#onOff").prop("checked", false);
-      $("#spinnerRow").hide();
-      $("#logRow").show();
-    }
-  });
-});
-
-$("#onOff").on("click", () => {
-  let onOff = $("#onOff").prop("checked");
-  console.log("user changed logging to " + onOff);
-  browser.runtime.getBackgroundPage().then((page) => {
-    page.ignoreNextWrite(); // Don't propagate changes the PAC script
-    setLogging(500, onOff).then(() => {
-      if (!onOff) {
-        logg.active = false;
-        logg.clear();
-        renderLog();
-      }
-      else logg.active = true;
-      getAllSettings().then((s) => console.log(s));
-    });
-  });
-});
-
-$("#okBtn1,#okBtn2").on("click", () => {
-  location.href = "/proxies.html";
-});
-
-$("#clearBtn1,#clearBtn2").on("click", () => {
-  logg.clear();
-  renderLog();
-});
-
-$("#refreshBtn1,#refreshBtn2").on("click", () => {
-  renderLog();
-});
-
-function renderLog() {
-  let rows = [];
-  for (let i=0; i<logg.length; i++) {
-    let item = logg.item(i), pattern;
-    if (item.matchedPattern) {
-      pattern = item.matchedPattern == USE_PROXY_FOR_ALL_URLS ? "Use proxy for all URLs" :
-        item.matchedPattern.pattern;
-    }
-    else pattern = "No matches";
-
-		// Build a row for this log entry
-		let row = document.createElement("tr");
-		row.setAttribute("class", item.matchedPattern ? "success" : "secondary");
-		let cell1 = document.createElement("td");
-		row.appendChild(cell1);
-		let a1 = document.createElement("a");
-		cell1.appendChild(a1);
-		a1.setAttribute("href", item.url);
-		a1.setAttribute("target", "_blank");
-		a1.appendChild(document.createTextNode(item.url));
-		let cell2 = document.createElement("td");
-		row.appendChild(cell2);
-		cell2.appendChild(document.createTextNode(item.proxySetting ? item.proxySetting.title : "No matches"));
-		let cell3 = document.createElement("td");
-		row.appendChild(cell3);
-		cell3.style.backgroundColor = item.proxySetting ? item.proxySetting.color : "blue";
-		cell3.setAttribute("class", "fp-color-blob-log");
-		let cell4 = document.createElement("td");
-		row.appendChild(cell4);
-		cell4.appendChild(document.createTextNode(item.proxySetting ? item.proxySetting.address : "No matches"));
-		let cell5 = document.createElement("td");
-		row.appendChild(cell5);
-		cell5.appendChild(document.createTextNode(pattern));
-		let cell6 = document.createElement("td");
-		row.appendChild(cell6);
-		cell6.appendChild(document.createTextNode(format(item.timestamp)));
-    rows.push(row);
-  }
-
-	let parent = document.getElementById("rows");
-	[...parent.childNodes].forEach(el => el.remove())
-	rows.forEach(row => parent.appendChild(row));
-  $("#spinnerRow").hide();
-  $("#logRow").show();
-}
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Thanks for the inspiration, Tor2k (http://www.codeproject.com/jscript/dateformat.asp)
 function format(d) {
+
   d = new Date(d);
-  if (!d.valueOf())
-    return ' ';
+  if (!d.valueOf()) { return ' '; }
+
   var self = this;
   return "HH:nn:ss:zzz".replace(/yyyy|mmmm|mmm|mm|dddd|ddd|dd|hh|HH|nn|ss|zzz|a\/p/gi,
     function($1) {
