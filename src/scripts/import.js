0ff784c5cd2ff644322f997b679a813d9eb8cfe5
erosman <erosman@users.noreply.github.com>
2020-08-18 19:29:13 +0430

    Update import.js
    i18n Utility function

--- src/scripts/import.js
+++ src/scripts/import.js
@@ -1,12 +1,8 @@
 'use strict';
 
 // ----------------- Internationalization ------------------
-document.querySelectorAll('[data-i18n]').forEach(node => {
-  let [text, attr] = node.dataset.i18n.split('|');
-  text = chrome.i18n.getMessage(text);
-  attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
-});
-// ----------------- /Internationalization -----------------
+Utils.i18n();
+
 document.addEventListener('keyup', evt => {
   if (evt.keyCode === 27) {
     close();
