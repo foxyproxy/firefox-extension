fa0dfab156d4c9a090900a73e8a9396c93c36c57
erosman <erosman@users.noreply.github.com>
2021-10-26 20:25:20 +0330

    Support for custom attributes

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -325,7 +325,7 @@ class Utils {
     document.querySelectorAll('[data-i18n]').forEach(node => {
       let [text, attr] = node.dataset.i18n.split('|');
       text = chrome.i18n.getMessage(text);
-      attr ? node[attr] = text : node.append(text);
+      attr ? node.setAttribute(attr, text) : node.append(text);
     });
   }  
 }
